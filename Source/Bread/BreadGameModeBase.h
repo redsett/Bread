df816a9fdb5c5f0fd623ec6d7cec19d8b77a4305
redsett <ko1o33@hotmail.com>
2023-01-28 10:04:38 -0800

    Initial commit

---
+++ Source/Bread/BreadGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BreadGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BREAD_API ABreadGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
