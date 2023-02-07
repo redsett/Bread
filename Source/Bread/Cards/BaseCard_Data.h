b82100e22e9a9f4746592ad4783cc1e11c6d26ee
Travis Stewart <travisstewart@gmx.com>
2023-02-06 19:17:36 -0800

    Created BaseCard_Data

---
+++ Source/Bread/Cards/BaseCard_Data.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "BaseCard_Data.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BREAD_API UBaseCard_Data : public UObject
+{
+	GENERATED_BODY()
+	
+};
